3d0a4da3556a8a2c7f910ce5d06fe889e2e9f29a
Jelena <jelenarankovic0@gmail.com>
2022-11-04 12:51:15 +0100

    global mixins

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 
-const app = createApp(App)
+import loggerMixing from './mixins/logger.js';
 
+const app = createApp(App);
+app.mixin(loggerMixing);
 app.mount('#app');
